480d46ecff953ebcdc3ca595741dbb06c659ef50
Jeremy Magland <jmagland@flatironinstitute.org>
2025-04-14 17:34:53 -0400

    gh pages

--- dandi-ai-notebooks-explorer/vite.config.ts
+++ dandi-ai-notebooks-explorer/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dandi-ai-notebooks-2/',
   plugins: [react()],
   server: {
     port: 3000,
